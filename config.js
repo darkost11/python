6eb7a4f5a0ca0007b2b5e57938f8847eb93f8659
= <lutiyxs21@gmail.com>
2026-01-03 21:58:03 +0300

    uncheck reset upgrades

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ let stopSnake = 0;
 let numRows = 12;
 let numCols = 8;
 const cellSize = 58;
-const RESET_UPGRADES = 1;
+const RESET_UPGRADES = 0;
 const RESET_PLAYER_DATA = 0;
 const STARTING_APPLES = 0;
   
